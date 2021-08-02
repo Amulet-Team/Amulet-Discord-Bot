a6647396e42e92a7863963ed30f32284fb5b0411
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-08-02 15:39:13 +0100

    Added a program to create the prof regex

---
+++ amulet_discord_bot/merge_prof.py
@@ -0,0 +1,22 @@
+import gzip
+import re
+
+
+def main():
+    with open("prof_database", encoding="utf-8") as f:
+        prof_in = [l.strip().lower() for l in f.readlines() if l.strip()]
+    prof = []
+    for l in prof_in:
+        prof.append(l)
+        if not l.endswith("s"):
+            prof.append(f"{l}s")
+    prof = sorted(set(prof))
+    with open("prof_database", "w", encoding="utf-8") as f:
+        f.write("\n".join(prof))
+    prof = [re.escape(l) for l in prof]
+    with gzip.open("prof", "wb") as f:
+        f.write(("(^| )(" + "|".join(prof) + ")( |\\.|$)").encode("utf-8"))
+
+
+if __name__ == '__main__':
+    main()
