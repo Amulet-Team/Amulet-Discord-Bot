b7ac04d041b351b0a646a75bf00ad98b4d4b0189
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-09-05 13:14:45 +0100

    Moved bot logging to a new chat

--- amulet_discord_bot/const.py
+++ amulet_discord_bot/const.py
@@ -8,7 +8,7 @@ class Servers(IntEnum):
 class Chats(IntEnum):
     AmuletGeneral = 498974201416253443
     AmuletPlugins = 856261919689932840
-    ServerLog = 407732480863371264
+    ServerLog = 884022949756153906
 
 
 HelpMessages = ["help", "help me", "can someone help me"]
